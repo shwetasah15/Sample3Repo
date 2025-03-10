068191e92750f215a326bdfe2091c0a2c9bd1ad6
shwetasah15 <shwetasah15@gmail.com>
2025-03-10 18:18:04 +0530

    add files to git hub

---
+++ Jenkins_Demo_10Mar/src/test/java/TestCases/DemoLogin.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class DemoLogin {
+
+    @Test  // This annotation is required for TestNG to detect and execute this method
+    public void testLogin() {
+        System.out.println("Hello, this is a TestNG test!");
+    }
+}
